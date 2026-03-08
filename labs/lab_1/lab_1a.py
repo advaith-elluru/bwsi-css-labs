a4390503e518d3da4de040459a159b2decb76d1e
Advaith Elluru <76271827+advaith-elluru@users.noreply.github.com>
2026-03-08 19:54:49 -0400

    Change robot speed from 5 to 8

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,7 +4,11 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
-This is to simulate a change made on a robot: robot_speed = 5 # m/s
+<<<<<<< HEAD
+This is to simulate a change made on a robot: robot_speed = 8 # m/s
+=======
+This is to simulate a change made on a robot: robot_speed = 3 # m/s
+>>>>>>> 7e239617b5aecf38cc1ba1199c53c5c68d2f1091
 """
 
 def main():
