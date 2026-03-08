f984297074f656fd51c358bb82e3054dc7879900
Advaith Elluru <76271827+advaith-elluru@users.noreply.github.com>
2026-03-08 19:58:23 -0400

    Update robot_speed value in lab_1a.py

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -4,11 +4,7 @@ lab_1a.py
 The first lab in the BWSI CSS course. To complete this lab, fill out the variable on line 10
 with your name. Then, save the code, add it to the staging area, and commit it to the Git tree.
 
-<<<<<<< HEAD
-This is to simulate a change made on a robot: robot_speed = 8 # m/s
-=======
 This is to simulate a change made on a robot: robot_speed = 3 # m/s
->>>>>>> 7e239617b5aecf38cc1ba1199c53c5c68d2f1091
 """
 
 def main():
