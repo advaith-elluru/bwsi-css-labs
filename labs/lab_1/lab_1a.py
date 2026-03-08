5467d3c7d48c066311639bd4b89029775c4a11ea
Advaith Elluru <76271827+advaith-elluru@users.noreply.github.com>
2026-03-08 19:29:20 -0400

    Added my description to lab 1a

--- labs/lab_1/lab_1a.py
+++ labs/lab_1/lab_1a.py
@@ -11,6 +11,7 @@ def main():
     name = "Advaith Elluru" # TODO: Insert your name between the double quotes
 
     print(f"{name}, Welcome to the CSS course!")
+    print("Hi everyone. My name is Advaith Elluru and I am a junior in Pennsylvania. A fun fact about me is that I play chess. Looking forward to this course.")
 
 if __name__ == "__main__":
     main()
